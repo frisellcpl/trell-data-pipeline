8864cb901a901582cbe9e5fadc4210d1929363db
Johan Frisell <johan@trell.se>
2020-02-10 10:03:00 +0100

    passing kwargs to client from factory

--- pipeline/clients/__init__.py
+++ pipeline/clients/__init__.py
@@ -1,11 +1,13 @@
+from typing import Any
+
 from .base import Client
 
 
-async def init_client(cls: Client) -> Client:
+async def init_client(cls: Client, **kwargs: Any) -> Client:
     '''
     Creates an instance and runs setup instructions of supplied client class.
     '''
-    client = cls()
+    client = cls(**kwargs)
     await client._setup()
     return client
 
