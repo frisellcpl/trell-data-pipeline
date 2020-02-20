8cd413eb387a5e55688310ebb36c08d0e61317d5
Johan Frisell <johan@trell.se>
2020-02-20 10:32:48 +0100

    extracted generation of mock data

--- pipeline/clients/mock.py
+++ pipeline/clients/mock.py
@@ -9,20 +9,23 @@ from .base import Client
 LOG = logging.getLogger(__name__)
 
 
+async def _gen_mock_data():
+    return {
+        'ts': int(time.time()),
+        'id': 'a81758fffe048c45',
+        't': 21.3,
+        'h': 42.1,
+        'b': 3021,
+        'c': 551.32,
+        'l': 15,
+        'm': 1,
+    }
+
+
 class MockClient(Client):
     async def _produce_mock_messages(self):
-        data = {
-            'ts': int(time.time()),
-            'id': 'a81758fffe048c45',
-            't': 21.3,
-            'h': 42.1,
-            'b': 3021,
-            'c': 551.32,
-            'l': 15,
-            'm': 1,
-        }
-
         while self.connected:
+            data = await _gen_mock_data()
             await self.on_message(self, data)
             await asyncio.sleep(5)
 
