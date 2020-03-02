bd1a3bf28bd0b8ed9e500642e36e539bcbc2c7df
Johan Frisell <johan@trell.se>
2020-03-02 20:36:38 +0100

    better mock data

--- pipeline/clients/mock.py
+++ pipeline/clients/mock.py
@@ -1,3 +1,4 @@
+import random
 import logging
 import asyncio
 import time
@@ -9,28 +10,53 @@ from .base import Client
 LOG = logging.getLogger(__name__)
 
 
-async def _gen_mock_data():
-    return {
-        'ts': int(time.time()),
-        'id': 'a81758fffe048c45',
-        't': 21.3,
-        'h': 42.1,
-        'b': 3021,
-        'c': 551.32,
-        'l': 15,
-        'm': 1,
-    }
+class MockDataGenerator():
+    def __init__(self):
+        self.sensor_ids = [
+            'a000000000000001',
+            'a000000000000002',
+            'a000000000000003',
+            'a000000000000004',
+            'a000000000000005',
+            'a000000000000006',
+        ]
+
+        self.data = self._init_data()
+
+    def _init_data(self):
+        return [
+            {
+                'ts': int(time.time()),
+                'id': sensor_id,
+                't': round(random.uniform(19.5, 22.5), 2),
+                'h': round(random.uniform(40.0, 50.0), 2),
+                'b': 3021,
+                'c': round(random.uniform(450, 900), 0),
+                'l': round(random.uniform(2, 150), 0),
+                'm': 1,
+            } for sensor_id in self.sensor_ids
+        ]
+
+    async def refresh(self):
+        for d in self.data:
+            d['t'] += round(random.uniform(-0.05, 0.05), 2)
+            d['h'] += round(random.uniform(-0.05, 0.05), 2)
+            d['c'] += round(random.uniform(-2, 2), 0)
+            d['l'] += round(random.uniform(-0.15, 0.15), 0)
 
 
 class MockClient(Client):
     async def _produce_mock_messages(self):
         while self.connected:
-            data = await _gen_mock_data()
-            await self.on_message(self, data)
-            await asyncio.sleep(5)
+            await self.mock_data_generator.refresh()
+            for data in self.mock_data_generator.data:
+                await self.on_message(self, data)
+
+            await asyncio.sleep(60)
 
     async def connect(self, topics: List) -> None:
         self.connected = True
+        self.mock_data_generator = MockDataGenerator()
         await self._produce_mock_messages()
         LOG.info('Mock client connected')
 
