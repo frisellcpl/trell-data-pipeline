f19621feae51541403b6391a3d7274cd8f0bad3b
Johan Frisell <johan@trell.se>
2020-04-29 12:46:12 +0200

    fixed some refs in amqp client

--- pipeline/clients/amqp.py
+++ pipeline/clients/amqp.py
@@ -11,11 +11,21 @@ from .base import Client
 LOG = logging.getLogger(__name__)
 
 
+def _on_disconnect(connection):
+    LOG.debug('%s: DISCONNECTED', connection)
+
+
+def _on_connect(connection):
+    LOG.debug('%s: CONNECTED', connection)
+
+
 class AMQPClient(Client):
     ''' Client for consuming records from a Kafka topic and
     pipe it to configured producer. '''
     def __init__(
             self,
+            on_connect: Callable = _on_connect,
+            on_disconnect: Callable = _on_disconnect,
             **kwargs: Any,
     ):
         super().__init__(**kwargs)
@@ -30,18 +40,18 @@ class AMQPClient(Client):
         self.channel = await self.connection.channel()
 
         for topic in topics:
-            deaclare_ok = await channel.queue_declare(topic)
-            consume_ok = await channel.basic_consume(
+            deaclare_ok = await self.channel.queue_declare(topic)
+            consume_ok = await self.channel.basic_consume(
                 deaclare_ok.queue,
                 self.on_message,
                 no_ack=False,
                 durable=True,
             )
         
-        self.on_connect(self.channel)
+        self.on_connect(self.connection)
         self.connected = True
 
     async def disconnect(self) -> None:
         await self.connection.close()
+        self.on_disconnect(self.connection)
         self.connected = False
-        self.on_disconnect(self.consumer._client)
