212b8eed162d65fa5bb29dc425eb067f489d92c9
Johan Frisell <johan@trell.se>
2020-04-29 10:58:16 +0200

    added firehose to available producers

--- pipeline/producers/__init__.py
+++ pipeline/producers/__init__.py
@@ -2,6 +2,7 @@ from pipeline import settings
 from .base import Producer
 from .kafka import KafkaProducer
 from .mock import MockProducer
+from .firehose import FirehoseProducer
 from .timescale import TimescaleProducer
 
 
@@ -22,5 +23,8 @@ async def get_producer() -> Producer:
 
     if settings.PRODUCER_DRIVER == 'timescale':
         return await create_producer(TimescaleProducer)
+
+    if settings.PRODUCER_DRIVER == 'aws_firehose':
+        return await create_producer(FirehoseProducer)
 
     return await create_producer(MockProducer)
