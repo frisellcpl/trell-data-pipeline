cdeb820e113f456f273af69f1516a15942c3ae7f
Johan Frisell <johan@trell.se>
2020-04-30 09:44:23 +0200

    movec queues back into client

--- pipeline/clients/amqp.py
+++ pipeline/clients/amqp.py
@@ -38,17 +38,21 @@ class AMQPClient(Client):
         self.connection = None
         self.connstring = f'amqp://{self.username}:{self.password}@{self.uri}'
 
-    async def connect(self, topics: str, no_ack: bool = True, durable: bool = True) -> tuple:
+    async def connect(self, topics: str, no_ack: bool = True, durable: bool = True, prefetch_count: int = 10) -> tuple:
         self.connection = await aiormq.connect(self.connstring)
 
         channel = await self.connection.channel()
-        await channel.basic_qos(prefetch_count=10)
-        queue = await channel.queue_declare(topics, durable=durable)
+        await channel.basic_qos(prefetch_count=prefetch_count)
+
+        declare_ok = await channel.queue_declare(topics, durable=durable)
+        consume_ok = await channel.basic_consume(
+            declare_ok.queue,
+            self.on_message,
+            no_ack=no_ack,
+        )
         
         self.on_connect(self.connection)
         self.connected = True
-
-        return channel, queue
 
     async def disconnect(self) -> None:
         await self.connection.close()
