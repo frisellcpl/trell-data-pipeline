4b7cab281d5422482f5574ca02443e2a40a474ca
Johan Frisell <johan@trell.se>
2020-09-10 11:24:44 +0200

    bumped version

--- setup.py
+++ setup.py
@@ -26,7 +26,6 @@ setup(
         'License :: Public Domain',
         'Natural Language :: English',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],
