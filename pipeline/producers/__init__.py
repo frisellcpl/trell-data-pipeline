761facc4b60e9975a3ae2c04d60f068fa9959785
Johan Frisell <johan@trell.se>
2020-12-15 13:21:01 +0100

    added support for http post

--- pipeline/producers/__init__.py
+++ pipeline/producers/__init__.py
@@ -4,6 +4,7 @@ from .kafka import KafkaProducer
 from .mock import MockProducer
 from .firehose import FirehoseProducer
 from .timescale import TimescaleProducer
+from .http import HttpProducer
 from .kinesis_data_stream import KinesisDataStreamProducer
 
 
@@ -30,5 +31,8 @@ async def get_producer() -> Producer:
 
     if settings.PRODUCER_DRIVER == 'aws_kinesis_data_stream':
         return await create_producer(KinesisDataStreamProducer)
+
+    if settings.PRODUCER_DRIVER == 'http':
+        return await create_producer(HttpProducer)
 
     return await create_producer(MockProducer)
---
+++ pipeline/producers/http.py
@@ -0,0 +1,33 @@
+import logging
+import json
+from typing import Any
+
+from aiohttp import ClientSession
+
+from .base import Producer
+
+
+LOG = logging.getLogger(__name__)
+
+
+class HttpProducer(Producer):
+    '''
+    Connects to a kafka cluster and roduces messages as an encoded JSON structure.
+    '''
+    async def _setup(self) -> None:
+        await super()._setup()
+
+    async def connect(self) -> None:
+        ''' Connect to producer destination '''
+        LOG.info('Connected.')
+
+    async def disconnect(self) -> None:
+        ''' Disconnect from producer destination '''
+        LOG.info('Disconnected.')
+
+    async def produce_data(self, data: dict, target: str = None) -> None:
+        await super().produce_data(data=data, target=target)
+
+        LOG.debug('Trying to produce data: %s', data)
+        async with ClientSession as session, session.post(target, json=data) as response:
+            await response.text()
